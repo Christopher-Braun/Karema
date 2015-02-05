eb23bd1381e265f6f5327413af131486d5d52f40
chribra1 <cbraun@gmx.li>
2015-02-05 17:46:00 +0100

    Revert "Models in extra Projekt. Files zu SourceControl hinzugefügt"
    This reverts commit dbae4e87ed741547f8a3dad533959bf702d86f4f.

--- KaremaCloud/Mvc4WebRole/Models/Defines.cs
+++ KaremaCloud/Mvc4WebRole/Models/Defines.cs
@@ -8,6 +8,8 @@ namespace Mvc4WebRole.Models
 {
     public static class Defines
     {
+        public const Int32 MaxPersonCount = 24;
+
         public static readonly CultureInfo Culture = new CultureInfo("de-DE");
     }
 }
